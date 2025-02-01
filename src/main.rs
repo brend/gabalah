a5c3e327e1046e465845b0e03819d901bfb2cb76
waldrumpus <waldrumpus@gmail.com>
2025-02-01 11:33:03 +0100

    add Byte, Word, Registers, Ram

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,78 @@
 mod app;
 
-use pixels::Error;
+/// An 8-bit unsigned integer
+#[derive(PartialEq, PartialOrd, Clone, Copy, Default)]
+pub struct Byte(u8);
 
-fn main() -> Result<(), Error> {
-    app::run_loop()
+impl std::fmt::Debug for Byte {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Byte {
+    fn zero() -> Byte {
+        Byte(0)
+    }
+}
+
+/// A 16-bit unsigned integer
+#[derive(PartialEq, PartialOrd, Clone, Copy, Default)]
+pub struct Word(u16);
+
+impl std::fmt::Debug for Word {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/// A struct representing the Game Boy's CPU registers
+#[derive(Default, Debug)]
+pub struct Registers {
+    /// accumulator A
+    a: Byte,
+    /// general purpose register B
+    b: Byte,
+    /// general purpose register D
+    d: Byte,
+    /// general purpose register H
+    h: Byte,
+    /// flags register F
+    f: Byte,
+    /// general purpose register C
+    c: Byte,
+    /// general purpose register E
+    e: Byte,
+    /// general purpose register L
+    l: Byte,
+    /// stack pointer
+    sp: Word,
+    /// program counter
+    pc: Word,
+}
+
+impl Registers {
+    /// returns an instance of Registers with every register set to 0
+    fn new() -> Registers {
+        Registers::default()
+    }
+}
+
+const RAM_SIZE: usize = 64 * 1024;
+
+/// A struct representing the Game Boy's random-access memory
+#[derive(Debug)]
+struct Ram {
+    cells: [Byte; RAM_SIZE],
+}
+
+impl Ram {
+    fn new() -> Ram {
+        Ram { cells: [Byte::zero(); RAM_SIZE] }
+    }
+}
+
+fn main()  {
+    let r = Ram::new();
+    println!("{:?}", r)
 }
