03132265d597201a938c93f27d18b8095cbbdbbf
waldrumpus <waldrumpus@gmail.com>
2025-02-03 18:50:45 +0100

    enum instructions

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod err;
 
 use std::collections::HashMap;
 
+use log::debug;
 use ram::{Ram, Registers, Addr};
 
 #[derive(Debug, Clone)]
@@ -15,35 +16,60 @@ enum Mnemonic {
 }
 
 #[derive(Debug, Clone)]
-enum Dest {
-    None,
+enum Location {
     A,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
     BC,
+    Operand8,
+    Operand16,
 }
 
-impl Dest {
+impl Location {
     fn write(&self, registers: &mut Registers, memory: &mut Ram, values: Vec<u8>) {
+        debug!("writing [{}] to {:?}", values.iter().map(|n|n.to_string()).collect::<Vec<String>>().join(", "), self);
         match self {
-            Dest::A => registers.a = values[0],
-            Dest::BC => registers.set_bc(values[0], values[1]),
+            Location::A => registers.a = values[0],
+            Location::BC => registers.set_bc(values[0], values[1]),
             _ => panic!()
         }
     }
 
-    // fn write16(&self, registers: &mut Registers, lo: u8, hi: u8) {
-    //     match self {
-    //         Dest::BC => registers.set_bc(lo, hi),
-    //         _ => panic!()
-    //     }
-    // }
+    fn read(&self, registers: &Registers, memory: &Ram) -> Vec<u8> {
+        match self {
+            Location::A => vec![registers.a],
+            Location::B => vec![registers.b],
+            Location::C => vec![registers.c],
+            Location::D => vec![registers.d],
+            Location::E => vec![registers.e],
+            Location::H => vec![registers.h],
+            Location::L => vec![registers.l],
+            Location::BC => vec![registers.c, registers.b],
+            Location::Operand8 => vec![memory.get(Addr(registers.pc).next().unwrap())],
+            Location::Operand16 => {
+                let op_pointer = Addr(registers.pc).next().unwrap();
+                vec![memory.get(op_pointer), memory.get(op_pointer.next().unwrap())]
+            },
+        }
+    }
+
+    fn len(&self) -> u16 {
+        match self {
+            Self::Operand8 => 1,
+            Self::Operand16 => 2,
+            _ => 0
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
-struct Instruction {
-    opcode: u8,
-    mnemonic: Mnemonic,
-    bytes: i32, 
-    dest: Dest,
+enum Instruction {
+    Nop,
+    Ld(Location, Location),
 }
 
 impl Instruction {
@@ -51,25 +77,18 @@ impl Instruction {
         opcode_map.get(&opcode).cloned()
     }
 
-    fn execute(&self, memory: &mut Ram, registers: &mut Registers) {
-        let operands = self.read_operands(memory, Addr(registers.pc));
-
-        match self.mnemonic {
-            Mnemonic::Nop => (),
-            Mnemonic::Ld => self.dest.write(registers, memory, operands),
-            Mnemonic::Inc => self.dest.write(self.src + 1),
-            Mnemonic::Dec => (),
+    fn len(&self) -> u16 {
+        1 + match self {
+            Self::Ld(dst, src) => dst.len() + src.len(),
+            _ => 0
         }
     }
 
-    fn read_operands(&self, memory: &Ram, location: Addr) -> Vec<u8> {
-        let mut operands = vec![];
-        let mut location = location;
-        for _ in 1..self.bytes {
-            location.inc();
-            operands.push(memory.get(location));
+    fn execute(&self, memory: &mut Ram, registers: &mut Registers) {
+        match self {
+            Self::Nop => (),
+            Self::Ld(dst, src) => dst.write(registers, memory, src.read(registers, memory)),
         }
-        operands
     }
 }
 
@@ -78,32 +97,17 @@ fn build_opcode_map() -> HashMap<u8, Instruction> {
 
     map.insert(
         0x00,
-        Instruction {
-            opcode: 0x00,
-            mnemonic: Mnemonic::Nop,
-            bytes: 1,
-            dest: Dest::None,
-        }
+        Instruction::Nop,
     );
 
     map.insert(
         0x01,
-        Instruction {
-            opcode: 0x01,
-            mnemonic: Mnemonic::Ld,
-            bytes: 3,
-            dest: Dest::BC,
-        }
+        Instruction::Ld(Location::BC, Location::Operand16),
     );
 
     map.insert(
         0x02,
-        Instruction {
-            opcode: 0x02,
-            mnemonic: Mnemonic::Ld,
-            bytes: 1,
-            dest: Dest::A,
-        }
+        Instruction::Ld(Location::BC, Location::A),
     );
 
     map
@@ -113,9 +117,22 @@ fn main()  {
     let opcode_map = build_opcode_map();
     let mut memory = Ram::new();
     let mut registers = Registers::new();
-    // ... time passes...
-    let mut pc = Addr(registers.pc);
-    let opcode = memory.get(pc);
-    let instruction = Instruction::decode(opcode, &opcode_map).unwrap();
-    instruction.execute(&mut memory, &mut registers);
+
+    memory.set(Addr(0), 0x01);
+    memory.set(Addr(1), 0x17);
+    memory.set(Addr(2), 0x04);
+    
+    registers.pc = 0;
+
+    for _ in 0..2 {
+        let pc = Addr(registers.pc);
+        println!("*** pc: {:?}", pc);
+        let opcode = memory.get(pc);
+        let instruction = Instruction::decode(opcode, &opcode_map).unwrap();
+        println!("*** instruction: {:?}", instruction);
+        instruction.execute(&mut memory, &mut registers);
+        println!("{:?}", registers);
+        registers.pc += instruction.len();
+        break;
+    }
 }
